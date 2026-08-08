703e49586eacbc9dbea6049bc7c112202233bf22
SatriaIndo <satriachandra0705@gmail.com>
2026-08-08 21:42:50 +0800

    Update pingpong_game.py
    Adding key movement, interaction between walls and ball, and racket with ball, and lastly adding a text when player left lose or player right lose

--- pingpong_game.py
+++ pingpong_game.py
@@ -35,7 +35,7 @@ class Racket(GameSprite):
             self.rect.y += self.speed
 
 racket1 = Racket('racket.png', 30, 200, 4, 50, 150)
-racket2 = Racket('racket.png', 520, 200, 4, 50, 150)
+racket2 = Racket('racket.png', 600, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 game = True
@@ -44,38 +44,55 @@ clock = time.Clock()
 
 speed_x = 3
 speed_y = 5
+finish = False
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('Left Player Lose!', True, (180, 0, 0))
+lose2 = font.render('Right Player Lose!', True, (180, 0, 0))
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-    window.fill(background_color)
+    if finish != True:
+        window.fill(background_color)
 
-    racket1.reset()
-    racket1.update()
-    racket1.update_l()
+        racket1.reset()
+        racket1.update()
+        racket1.update_l()
 
-    racket2.reset()
-    racket2.update()
-    racket2.update_r()
+        racket2.reset()
+        racket2.update()
+        racket2.update_r()
 
-    ball.reset()
-    ball.update()
+        ball.reset()
+        ball.update()
 
-    # Bola bergerak horizontal karena 2 program ini yang bekerja secara bersamaan
-    ball.rect.x += speed_x
-    ball.rect.y += speed_y
+        # Bola bergerak horizontal karena 2 program ini yang bekerja secara bersamaan
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-    # Bola ketika terkena dinding maka akan memantul
-    if ball.rect.y > height - 10 or ball.rect.y < 0:
-        speed_y *= -1
+        # Bola ketika terkena dinding maka akan memantul
+        if ball.rect.y > height - 10 or ball.rect.y < 0:
+            speed_y *= -1
 
-    # Bola mantul pas kena raket
-    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-        speed_x *= -1
-        speed_y *= -1
+        # Bola mantul pas kena raket
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1
+
+        # Program jika player kiri kalah
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        # Program jika player kanan kalah
+        if ball.rect.x > width:
+            finish = True
+            window.blit(lose2, (200, 200))
 
 
-    display.update()
+        display.update()
     clock.tick(FPS)
